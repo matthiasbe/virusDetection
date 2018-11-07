22f4a901e321752668939d0de5510100bb7a8b31
Pierre GRANGER <pierre.granger@ens-cachan.fr>
2018-11-07 17:22:18 +0100

    Adding file opening to preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+import numpy as np
+import csv
+def get_data(datafile, ftcount):
+	data = list(csv.reader(open(datafile), delimiter=' '))
+	train = np.zeros((len(data),ftcount))
+	# converts "15:1" to row[15] = 1
+	idx = 0
+	for row in data:
+	    current = train[idx]
+	    current[ftcount-1] = int(row[0])
+	    for elt in row[1:]:
+	        split = elt.split(':')
+	        if (len(split) == 2):
+	            ftnum = int(split[0])
+	            if (ftnum <= len(current)-1):
+	                current[ftnum] = int(split[1])
+	    idx = idx + 1
+	return train
