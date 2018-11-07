dd9b5c3ac952ca8381f3eb94fdfa781162581b39
Pierre GRANGER <pierre.granger@ens-cachan.fr>
2018-11-07 17:44:46 +0100

    Adding things to postprocessing

--- postprocessing.py
+++ postprocessing.py
@@ -12,3 +12,8 @@ def correct_prediction(prediction, expected):
     assert(len(prediction) == len(expected))
     n = np.sum(prediction == expected)
     return float(n)/len(prediction)
+
+def evaluate(prediction, expected):
+	print("False positive : {0:.2f}".format(false_positive(prediction, expected)))
+	print("Missed positive : {0:.2f}".format(missed_positive(prediction, expected)))
+	print("Correct prediction : {0:.2f}".format(correct_prediction(prediction, expected)))
--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 import pandas as pd
-import numpy as np
 import csv
 import matplotlib.pyplot as plt
 import sklearn
 import preprocessing
 import postprocessing
 import vis
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import svm
 
 ftcount = 531
 
@@ -14,16 +16,14 @@ datafile = 'Dataset/dataset.train'
 train = preprocessing.get_data(datafile, ftcount)
 
 trainm = preprocessing.mask_unused_features(train)
-#train_masked = train[mask]
-#print(train_masked.shape)
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-trainm_train, trainm_test = train_test_split(trainm, train_size=0.9, test_size=0.1)
-clf = LogisticRegression(solver='lbfgs', max_iter=1000).fit(trainm_train[:,:-1], trainm_train[:,-1])
-clf2 = svm.SVC(gamma=0.001, C=100).fit(trainm_train[:,:-1], trainm_train[:,-1])
-print(clf.score(trainm_test[:,:-1], trainm_test[:,-1]))
-print(clf2.score(trainm_test[:,:-1], trainm_test[:,-1]))
+
+train, test = train_test_split(trainm, train_size=0.9, test_size=0.1)
+clf = LogisticRegression(solver='lbfgs', max_iter=1000).fit(train[:,:-1], train[:,-1])
+prediction = clf.predict(test[:,:-1])
+postprocessing.evaluate(prediction, test[:,-1])
+#clf2 = svm.SVC(gamma=0.001, C=100).fit(trainm_train[:,:-1], trainm_train[:,-1])
+#print(clf.score(trainm_test[:,:-1], trainm_test[:,-1]))
+#print(clf2.score(trainm_test[:,:-1], trainm_test[:,-1]))
 #virus_proportion = (np.sum(trainm[:,-1])/trainm.shape[0])
 #print(virus_proportion)
 #clf.predict(trainm_test[:,:-1])
